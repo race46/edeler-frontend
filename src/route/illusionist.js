6b310d821a24e7f375ede6b8c489a5a6e77cd18e
muhammed <muhammedyildiz.maras@gmail.com>
2023-02-23 23:02:27 +0300

    yes yes yes glay

--- src/route/illusionist.js
+++ src/route/illusionist.js
@@ -2,13 +2,71 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import Navbar from "./component/navbar";
 
+function getCurrenPrice(algo) {
+    if (!algo || !algo.pair) return ''
+    const ticker = JSON.parse(localStorage.getItem("ticker"))
+    const price = ticker.find(o => o.symbol === algo.pair)
+    return price.price
+}
+
+function getDate(id) {
+    const min = Math.ceil((Date.now() - (parseInt(id.substring(0, 8), 16) * 1000)) / 1000 / 60)
+    return `${Math.floor(min / 60 / 24)}d ${Math.floor(min / 60) % 24}h ${min % 60}m`
+}
+
+function calculateProfit(order, price, algo) {
+    if(order.buy) return '----'
+    const rate = ((price/order.buying*100 - 100)).toFixed(2)
+
+
+    return <span style={{color: rate>0?'green': 'red'}}>%{rate} </span>
+}
+
+function getFixedSelling(buying, selling) {
+    const b = buying.toString()
+    if (!b.includes(".")) return selling.toFixed(0)
+    return selling.toFixed(b.split('.')[1].length)
+}
+
+function getRow(algo) {
+    if (!algo.orders) return (<></>)
+    return (
+        algo.orders.map(o => {
+            return <tr key={o._id}>
+                <td>
+                    <div className="d-flex align-items-center">
+                        <img
+                            src='https://raw.githubusercontent.com/ErikThiart/cryptocurrency-icons/master/128/binance-coin.png'
+                            className="rounded-circle"
+                            alt=""
+                            style={{width: "45px", height: "45px"}}
+                        />
+                        <div className="ms-3">
+                            <p className="fw-bold mb-1"
+                               style={{color: o.buy ? 'green' : 'red'}}>{o.buy ? 'BUY - ' : 'SELL - '}</p>
+                            <p className="text-muted mb-0">{calculateProfit(o, getCurrenPrice(algo), algo)}</p>
+                        </div>
+                    </div>
+                </td>
+                <td className="mb-3">
+                    <p className="fw-normal mb-1"><span style={{color: 'green'}}>${o.buying}</span> - <span
+                        style={{color: 'red'}}>${getFixedSelling(o.buying, o.selling)}</span></p>
+                    <p className="text-muted mb-0">{getDate(o._id)}</p>
+                </td>
+            </tr>
+
+        })
+    )
+}
+
+
 export default () => {
     const {id} = useParams()
     const [algo, set_algo] = useState({})
     useEffect(() => {
         fetch('/api/illusionist/' + id).then(r => r.json()).then(r => {
-            const min = Math.ceil((Date.now() - (parseInt(r._id.substring(0, 8), 16) * 1000))/1000/60)
-            r.date = `${Math.floor(min/60/24)}d ${Math.floor(min/60) % 24}h ${min%60}m`
+            const min = Math.ceil((Date.now() - (parseInt(r._id.substring(0, 8), 16) * 1000)) / 1000 / 60)
+            r.date = `${Math.floor(min / 60 / 24)}d ${Math.floor(min / 60) % 24}h ${min % 60}m`
             set_algo(r)
         })
     }, [])
@@ -34,31 +92,16 @@ export default () => {
                                 </div>
                             </div>
                             <div className="card-body ">
-                                <h5 className="card-title mb-4">Order list {algo.date}</h5>
 
-                                <tbody>
-                                <tr>
-                                    <td>
-                                        <div className="d-flex align-items-center">
-                                            <img
-                                                src='https://raw.githubusercontent.com/ErikThiart/cryptocurrency-icons/master/128/binance-coin.png'
-                                                className="rounded-circle"
-                                                alt=""
-                                                style={{width: "45px", height: "45px"}}
-                                            />
-                                            <div className="ms-3">
-                                                <p className="fw-bold mb-1">{algo.pair}</p>
-                                                <p className="text-muted mb-0">${algo.amount}</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p className="fw-normal mb-1">{'devamke'} free</p>
-                                        <p className="text-muted mb-0">{'lclkfsdalkfjsda'} locked</p>
-                                    </td>
-                                </tr>
+                                <h5 className="card-title ">Order list {algo.date}</h5>
+                                <h5 className="card-title mb-4">${getCurrenPrice(algo)}</h5>
 
-                                </tbody>
+                                <table>
+                                    <tbody>
+                                    {getRow(algo)}
+
+                                    </tbody>
+                                </table>
                             </div>
 
                         </div>
